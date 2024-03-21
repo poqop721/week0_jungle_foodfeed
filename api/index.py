aaffe7657b3afd82c5b6186b66553cfb3ded0775
poqop721 <poqop721@naver.com>
2024-03-21 20:16:48 +0900

    fix : changed mongodb port

--- api/index.py
+++ api/index.py
@@ -9,8 +9,7 @@ import os
  
 app = Flask("Junge Food Feed")
 load_dotenv()
-test = 'mongodb://'+os.environ.get('DB_ID')+':'+os.environ.get('DB_PW')+'@'+os.environ.get('DB_IP')
-client = MongoClient('mongodb://'+os.environ.get('DB_ID')+':'+os.environ.get('DB_PW')+'@'+os.environ.get('DB_IP'), 32)
+client = MongoClient('mongodb://'+os.environ.get('DB_ID')+':'+os.environ.get('DB_PW')+'@'+os.environ.get('DB_IP'), 27017)
 dblog = client.jungle_food_feed #db명
 
 app.config.update(
@@ -24,10 +23,6 @@ app.secret_key = str(random.randrange(1, 100000))
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   global test
-   print(test)
-   # load_dotenv()
-   print('asdasda'+str(os.environ.get('TEST')))
    return render_template('login.html')
 
 @app.route('/main')
